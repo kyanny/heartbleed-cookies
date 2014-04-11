0e400f22400078915ab3841a4210e0fc4ed4e1c1
Kensuke Nagae <kyanny@gmail.com>
2014-04-11 19:21:33 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeartbleedCookies::Application.config.secret_key_base = '504ab07547917607d0139e3ccd6e3f0af21cf1d92d2983a476ab5f95846e579a5caf4fa8d768dcc56aca28b8a425f7d3b4e8673ad91f932e2d7299eb926d1e60'
