cfebe6c9b1a2341441fee9d7a130a55487e40da0
Kensuke Nagae <kyanny@gmail.com>
2014-04-11 19:47:56 +0900

    Set domain (.herokuapp.com)

--- app/controllers/heartbleed_controller.rb
+++ app/controllers/heartbleed_controller.rb
@@ -2,17 +2,21 @@ class HeartbleedController < ApplicationController
   def index
     cookies[:a] = {
       value: "A",
+      domain: :all,
     }
     cookies[:httponly] = {
       value: "HttpOnly",
+      domain: :all,
       httponly: true,
     }
     cookies[:secure] = {
       value: "Secure",
+      domain: :all,
       secure: true,
     }
     cookies[:httponlysecure] = {
       value: "HttpOnlySecure",
+      domain: :all,
       httponly: true,
       secure: true,
     }
