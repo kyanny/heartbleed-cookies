0504b5e06403b472722064ca3413bee2759ab6b6
Kensuke Nagae <kyanny@gmail.com>
2014-04-11 19:50:22 +0900

    Domains

--- app/controllers/heartbleed_controller.rb
+++ app/controllers/heartbleed_controller.rb
@@ -1,21 +1,39 @@
 class HeartbleedController < ApplicationController
   def index
-    cookies[:a] = {
-      value: "A",
+    cookies[:a1] = {
+      value: "A1",
+    }
+    cookies[:httponly1] = {
+      value: "HttpOnly1",
+      httponly: true,
+    }
+    cookies[:secure1] = {
+      value: "Secure1",
+      secure: true,
+    }
+    cookies[:httponlysecure1] = {
+      value: "HttpOnlySecure1",
+      httponly: true,
+      secure: true,
+    }
+
+    # Cookies domain: .herokuapp.com
+    cookies[:a2] = {
+      value: "A2",
       domain: :all,
     }
-    cookies[:httponly] = {
-      value: "HttpOnly",
+    cookies[:httponly2] = {
+      value: "HttpOnly2",
       domain: :all,
       httponly: true,
     }
-    cookies[:secure] = {
-      value: "Secure",
+    cookies[:secure2] = {
+      value: "Secure2",
       domain: :all,
       secure: true,
     }
-    cookies[:httponlysecure] = {
-      value: "HttpOnlySecure",
+    cookies[:httponlysecure2] = {
+      value: "HttpOnlySecure2",
       domain: :all,
       httponly: true,
       secure: true,
